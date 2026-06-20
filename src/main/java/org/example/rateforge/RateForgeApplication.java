2f947a5bd9734581323c9db689f337fda69748da
Shivansh Tiwari <stshivansh354@gmail.com>
2026-06-20 23:47:45 +0530

    dockerization done 2

--- src/main/java/org/example/rateforge/RateForgeApplication.java
+++ src/main/java/org/example/rateforge/RateForgeApplication.java
@@ -9,6 +9,7 @@ public class RateForgeApplication {
     public static void main(String[] args) {
         System.out.println("hello");
         SpringApplication.run(RateForgeApplication.class, args);
+        System.out.println("yess");
     }
 
 }
